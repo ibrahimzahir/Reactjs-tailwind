db8082311a50396ab868d4673de8573a0ca3b23b
Ibrahim Zahir <74900384+ibrahimzahir@users.noreply.github.com>
2021-10-06 14:26:29 +0500

    logo removed

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ ReactDOM.render(<App />, document.getElementById("root"));
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Tailwind
-        </a>
-      </header>
+      <header className="App-header">Learn React with Tailwind</header>
     </div>
   );
 }
